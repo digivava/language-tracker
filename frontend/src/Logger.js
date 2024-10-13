0dd6940d6925623ddaa485cba1c50d3e1ac06d03
VAL <val.conklin@gmail.com>
2024-10-13 13:35:01 -0700

    More instructions clarity, more language options

--- frontend/src/Logger.js
+++ frontend/src/Logger.js
@@ -7,9 +7,20 @@ export default function Logger({ onSubmit }) {
           }}>
               <p>I studied{' '}
                   <select name="language">
+                      <option value="Arabic">Arabic</option>
+                      <option value="Amharic">Amharic</option>
+                      <option value="Cantonese">Cantonese</option>
+                      <option value="Dutch">Dutch</option>
+                      <option value="French">French</option>
+                      <option value="German">German</option>
                       <option value="Japanese">Japanese</option>
+                      <option value="Korean">Korean</option>
                       <option value="Mandarin">Mandarin</option>
+                      <option value="Norwegian">Norwegian</option>
+                      <option value="Persian">Persian</option>
                       <option value="Russian">Russian</option>
+                      <option value="Spanish">Spanish</option>
+                      <option value="Vietnamese">Vietnamese</option>
                   </select>{' '}
                   for <input type="text" name="number"></input>{' '}
                   <select name="units">
